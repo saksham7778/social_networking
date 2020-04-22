9f30deab834af9b16ee5199417e1fe5ca54bfc49
saksam agarwal <saksamagr@gmail.com>
2020-04-22 23:14:11 +0530

    setup  mongoDB as database

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 const app = express();
-
-const expressLayouts = require('express-ejs-layouts');
-
 app.use(express.static('./assets'));
 app.use(expressLayouts);
 
@@ -12,11 +11,11 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-app.use('/',require('./routes/index'));
-
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use('/',require('./routes/index'));
 
 app.listen(port, function(err){
     if (err){
