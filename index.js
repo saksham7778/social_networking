6aeda3b14f1fee98db17dbbf5ca72032940c77fd
saksam agarwal <saksamagr@gmail.com>
2020-04-22 23:05:08 +0530

    basic folder structure completed

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const app = express();
 const port = 8000;
+
+const app = express();
+
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use('/',require('./routes/index'));
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if (err){
         console.log('Error in running the server: ',err);
     }
-
     console.log('Server is running on port: ',port);
-}); 
+});
