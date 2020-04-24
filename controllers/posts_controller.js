fc24cb37eb45a33e56076eb8bc5d4c88965e0002
saksam agarwal <saksamagr@gmail.com>
2020-04-24 12:49:29 +0530

    added delete option to posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/posts');
+const Comment = require('../models/comments');
 
 module.exports.create = function(req, res){
 
@@ -12,4 +13,22 @@ module.exports.create = function(req, res){
         return res.redirect('back');
     });
 
+}
+
+module.exports.destroy = async function(req, res){  
+
+    Post.findById(req.params.id , function(err,post){
+
+        // .id means converting the object id into string (req.user.id=> req.user._id) 
+        if(post.user == req.user.id){ // if the logged in user is same as the posted user or not 
+            post.remove();
+            Comment.deleteMany({post: req.params.id},function(err){
+                return res.redirect('back');
+            });
+        }
+        else{
+            console.log('you can not delete this post');
+            return res.redirect('back');
+        }
+    });
 }
