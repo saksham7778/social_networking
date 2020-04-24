477e139a4500a552b20ea40b9fbc436b3bd9f9e9
saksam agarwal <saksamagr@gmail.com>
2020-04-24 16:20:56 +0530

    converted home controller to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,24 +1,30 @@
 const Post =  require('../models/posts');
 const User =  require('../models/user');
-module.exports.home = function(req,res){
+
+module.exports.home = async function(req,res){
     
-    //// populate the user of each post
-    Post.find({})
-    .sort('-createdAt')
-    .populate('user')
-    .populate({
-        path:'comments', //from models/post.js
-        populate:{
-            path:'user'
-        }
-    })  
-    .exec(function(err,posts){
-        User.find({},function(err,users){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts:posts,
-                all_users:users,
-            });
+    try {
+        //// populate the user of each post
+        let posts =await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path:'comments', //from models/post.js
+            populate:{
+                path:'user'
+            }
         });
-    });
+        
+        let users = await User.find({});
+        
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:posts,
+            all_users:users,
+        });
+    } catch (error) {
+        console.log('Error in home_ontroller',error);
+        return;    
+    }
+
 } 
