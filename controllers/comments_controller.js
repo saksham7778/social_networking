c3e0cdc44169845ef10a499e916258233ad49ccb
saksam agarwal <saksamagr@gmail.com>
2020-04-24 17:42:55 +0530

    added async await

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,46 +1,48 @@
 const Comment = require('../models/comments');
 const Post = require('../models/posts');
 
-module.exports.create = function(req, res){
+module.exports.create = async function(req, res){
 
-Post.findById(req.body.post,function(err,post){ //in req.body.post (post is the name of input in home.ejs in the form)
-
-    if(post){
-        Comment.create({
-            content:req.body.content,
-            post:req.body.post,
-            user:req.user._id
-        },function(err,comment){
-            // handle error
-            if(err) {
-                console.log("error in creating comment");
-            }
-
+    try {
+        
+        let post = await Post.findById(req.body.post);
+        //in req.body.post (post is the name of input in home.ejs in the form)
+        if(post){
+            let comment = await Comment.create({
+                content:req.body.content,
+                post:req.body.post,
+                user:req.user._id
+            });
             // adding comment to post
             post.comments.push(comment); ///push() ->given by mongoDb    ,comments from model/post.js
             post.save();
-
             return res.redirect('/');
-        });
+        }
+
+    } catch (error) {
+        
+        console.log('Error in comment_controller-create : ' , error);
+        return res.redirect('back');
+
     }
-});
+    
 }
 
 
 module.exports.destroy = async function(req, res){
 
-    Comment.findById(req.params.id,function(err,comment){ 
-        if(comment.user == req.user.id ){
+    let comment =await  Comment.findById(req.params.id); 
 
-            let postId=comment.post;
-            comment.remove();
-            console.log('comment deleted');
-            Post.findByIdAndUpdate(postId , { $pull:{ comments: req.params.id }} , function(err,post){
-                return res.redirect('back');
-            })
-        }else{
-            console.log('you can not delete this comment');
+    if(comment.user == req.user.id ){
+
+        let postId=comment.post;
+        comment.remove();
+        console.log('comment deleted');
+        await Post.findByIdAndUpdate(postId , { $pull:{ comments: req.params.id }} , function(err,post){
             return res.redirect('back');
-        }
-    });
+        })
+    }else{
+        console.log('you can not delete this comment');
+        return res.redirect('back');
+    } 
 } 
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -4,29 +4,34 @@ const Comment = require('../models/comments');
 module.exports.create = async function(req, res){
 
     try {
+        
         await Post.create({
             content:req.body.content,
             user:req.user._id
         });
-        console.log("post created");
+        console.log('success Post created!');
         return res.redirect('back');
 
     } catch (error) {
-        console.log("error in creating post");
+        
+        // req.flash('error',error);
+        console.log('Error in post_controller-create : ' , error);
         return res.redirect('back');
+
     }
+        
 }
 
 module.exports.destroy = async function(req, res){  
-
+    
     try {
         
-        let post = Post.findById(req.params.id );
+        let post = await Post.findById(req.params.id);
 
         // .id means converting the object id into string (req.user.id=> req.user._id) 
         if(post.user == req.user.id){ // if the logged in user is same as the posted user or not 
             post.remove();
-            Comment.deleteMany({post: req.params.id},function(err){
+            await Comment.deleteMany({post: req.params.id},function(err){
                 return res.redirect('back');
             });
         }
@@ -37,9 +42,9 @@ module.exports.destroy = async function(req, res){
 
     } catch (error) {
         
-        console.log('Error in post_controller-destroy : ' , error);
-        return res.redirect('back');
-
+         // req.flash('error',error);
+         console.log('Error in post_controller-destroy : ' , error);
+         return res.redirect('back');
+         
     }
-
 }
